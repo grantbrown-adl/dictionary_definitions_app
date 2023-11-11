df93014c1150db41767a5e74cd6b588b79b68181
Grant Brown <grant.brown@outlook.com>
2023-11-11 23:36:29 +1030

    adds models, constants and dependencies
     - Added app dependencies for API calls
     - Added models to define incoming json
     - Added app constants to handle api url

---
+++ lib/models/dictionary_entry.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+
+List<DictionaryEntry?> dictionaryEntryFromJson(String? str) =>
+    (json.decode(str ?? "") as List<dynamic>?)
+        ?.map((x) => DictionaryEntry?.fromJson(x as Map<String, dynamic>?))
+        .toList() ??
+    <DictionaryEntry>[];
+
+String? dictionaryEntryToJson(List<DictionaryEntry?>? data) =>
+    json.encode(List<dynamic>.from(data?.map((x) => x?.toJson()) ?? []));
+
+class DictionaryEntry {
+  String word;
+  List<Meaning?> meanings;
+
+  DictionaryEntry({
+    required this.word,
+    required this.meanings,
+  });
+
+  String get firstDefinition =>
+      meanings.first?.definitions?.first?.definition ?? '';
+
+  bool get meaningsExist => meanings.isNotEmpty && meanings.first != null;
+
+  bool get definitionsExist =>
+      meanings.first!.definitions != null &&
+      meanings.first!.definitions != null;
+
+  bool get isDefined => meaningsExist && definitionsExist;
+
+  factory DictionaryEntry.fromJson(Map<String, dynamic>? json) =>
+      DictionaryEntry(
+        word: json?["word"] ?? '',
+        meanings: (json?["meanings"] as List<dynamic>?)
+                ?.map((x) => Meaning?.fromJson(x as Map<String, dynamic>?))
+                .toList() ??
+            [],
+      );
+
+  Map<String, dynamic>? toJson() => {
+        "word": word,
+        "meanings": List<dynamic>.from(meanings.map((x) => x?.toJson())),
+      };
+}
+
+class Meaning {
+  String? partOfSpeech;
+  List<Definition?>? definitions;
+
+  Meaning({
+    required this.partOfSpeech,
+    required this.definitions,
+  });
+
+  factory Meaning.fromJson(Map<String, dynamic>? json) => Meaning(
+        partOfSpeech: json?["partOfSpeech"],
+        definitions: (json?["definitions"] as List<dynamic>?)
+            ?.map((x) => Definition?.fromJson(x as Map<String, dynamic>?))
+            .toList(),
+      );
+
+  Map<String, dynamic>? toJson() => {
+        "partOfSpeech": partOfSpeech ?? '',
+        "definitions":
+            List<dynamic>.from(definitions?.map((def) => def?.toJson()) ?? []),
+      };
+}
+
+class Definition {
+  String? definition;
+  String? example;
+
+  Definition({
+    required this.definition,
+    this.example,
+  });
+
+  factory Definition.fromJson(Map<String, dynamic>? json) => Definition(
+        definition: json?["definition"] ?? '',
+        example: json?["example"],
+      );
+
+  Map<String, dynamic>? toJson() => {
+        "definition": definition ?? '',
+        "example": example ?? '',
+      };
+}
